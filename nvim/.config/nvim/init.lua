8c939db023d476490a631eafe2eba9c3a19349ee
A7R7 <Aaron__Lee_@outlook.com>
2023-06-23 22:51:13 +0800

    move nvim to submodule

--- nvim/.config/nvim/init.lua
+++
@@ -1,9 +0,0 @@
-local function reload_module(module_name)
-    package.loaded[module_name] = nil
-    return require(module_name)
-end
-reload_module("core.options")
-reload_module("core.lazy")
-reload_module("core.keymaps")
-reload_module("core.autocmds")
-
